f1dd432e81c31fc0c9c408f0f655ed855bbd6a09
RodrigueBW <rodrigue@better-world.io>
2025-09-27 19:43:24 +0200

    fix requirements

---
+++ setup_pysha_stub/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="pysha3",
+    version="1.0.99",
+    py_modules=["sha3"],
+)
---
+++ setup_pysha_stub/sha3.py
@@ -0,0 +1,6 @@
+import hashlib
+
+sha3_224 = hashlib.sha3_224
+sha3_256 = hashlib.sha3_256
+sha3_384 = hashlib.sha3_384
+sha3_512 = hashlib.sha3_512
